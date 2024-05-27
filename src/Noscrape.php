c1ed63950c72c857ce3d79808c1d5f7ad819e1d5
Bernhard Schönberger <bernhard.schoenberger@leebmann.de>
2024-05-27 22:34:18 +0200

    improve code documentations

--- src/Noscrape.php
+++ src/Noscrape.php
@@ -4,12 +4,28 @@ namespace Noscrape\Noscrape;
 
 use Exception;
 
+/**
+ * Class Noscrape
+ *
+ * Provides functionality to obfuscate text using a specified font and render it using an external library.
+ */
 class Noscrape {
 
     use NoscrapeLibraryLoader;
 
+    /**
+     * @var string Path to the font file.
+     */
     private string $font;
+
+    /**
+     * @var array Mapping of original characters to PUA characters.
+     */
     private array $mapping = [];
+
+    /**
+     * @var array Range of Private Use Area (PUA) Unicode characters.
+     */
     private array $puaRange;
 
     /**
@@ -25,27 +41,33 @@ class Noscrape {
         }
 
         $this->font = $font;
-        $this->puaRange = range(0xE000, 0xF8FF);
+        $this->puaRange = range(0xE000, 0xF8FF);  // Define the range of PUA characters
     }
 
     /**
      * Obfuscates a given string by mapping each character to a unique PUA (Private Use Area) character.
+     *
+     * This method ensures that each character in the input string is replaced by a unique PUA character,
+     * selected randomly from the available PUA characters that haven't been used yet.
      *
      * @param string $s The string to be obfuscated.
      * @return string The obfuscated string.
      */
     public function obfuscate(string $s): string
     {
+        // Available characters are those in the PUA range that haven't been mapped yet
         $availableChars = array_diff($this->puaRange, array_values($this->mapping));
 
         $obfuscated = '';
         foreach (str_split($s) as $c) {
+            // If the character hasn't been mapped yet, map it to a random available PUA character
             if (!isset($this->mapping[$c])) {
                 $randomIndex = array_rand($availableChars);
                 $this->mapping[$c] = $availableChars[$randomIndex];
                 unset($availableChars[$randomIndex]);
             }
 
+            // Convert the PUA character code to a UTF-8 encoded character and append it to the result
             $obfuscated .= mb_convert_encoding('&#' . $this->mapping[$c] . ';', "utf8", "html-entities");
         }
 
@@ -55,19 +77,24 @@ class Noscrape {
     /**
      * Renders the obfuscated text using noscrape binary.
      *
+     * This method serializes the font path and the character mapping into JSON,
+     * then uses a shell command to call the noscrape binary for rendering.
+     *
      * @return string The result of the rendering process.
      * @throws Exception if the rendering library fails to load.
      */
     public function render(): string
     {
+        // Load the external rendering library
         $binary = self::loadLib();
 
+        // Prepare the parameters for the rendering library
         $param = json_encode([
             "font" => $this->font,
             "translation" => $this->mapping,
         ]);
 
-        $command = "{$binary} '{$param}'";
-        return shell_exec($command);
+        // Execute the rendering command and return the output
+        return shell_exec( "{$binary} '{$param}'");
     }
 }
